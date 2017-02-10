0b1d91ef619709f4eced721322a3e59288cf3930
bug1024 <bug1024.com@gmail.com>
2017-02-10 13:01:43 +0800

    add simple_html_dom

--- test.php
+++ test.php
@@ -1,8 +1,11 @@
 <?php
 
 require_once __DIR__ . '/libs/Request.php';
-require_once __DIR__ . '/libs/HtmlDom.php';
+require_once __DIR__ . '/libs/simple_html_dom.php';
 
 $html = (new Request)->get('http://www.baidu.com');
-$r = (new HtmlDom($html))->find('.s_tab');
+$dom = new simple_html_dom();
+$dom->load($html);
+$find = $dom->find('title');
+$r = $find[0]->innertext();
 var_dump($r);
